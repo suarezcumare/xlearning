2f06592ee68b987f89c93b378e2bb8aa9992b988
Valentina Maldonado <c.valentinamm@gmail.com>
2015-01-09 11:51:00 -0430

    franja gris para el titulo

--- app/controllers/portal_controller.rb
+++ app/controllers/portal_controller.rb
@@ -1,4 +1,5 @@
 class PortalController < ApplicationController
 	def index
+		#if current_usuario.rol_actual == 	
 	end
 end
--- app/helpers/menu_helper.rb
+++ app/helpers/menu_helper.rb
@@ -4,15 +4,15 @@ module MenuHelper
 		menu.opcionmenu.each do |item|
 			if item.hijos.length > 0
 				codigo << "<li><a href='#submenu-#{item.id}' class='submenu-trigger' data-toggle='collapse'>"
-				codigo << "<i class='#{item.icono.to_s}'></i> #{item.nombre}</a>"
+				codigo << "<i class='#{item.icono.to_s}'></i> <span>#{item.nombre}</span></a>"
 				codigo << "<div id='submenu-#{item.id}' class='collapsible collapse'>"
 				codigo << "<ul class='submenu'>"
 				item.hijos.each do |hijo|
-					codigo << "<li><a href='#{hijo.url}'><i class='#{hijo.icono.to_s}'></i> #{hijo.nombre}</a></li>"
+					codigo << "<li><a href='#{hijo.url}'><i class='#{hijo.icono.to_s}'></i> <span>#{hijo.nombre}</span></a></li>"
 				end
 				codigo << "</ul></div></li>"
 			else
-				codigo << "<li><a href='#{item.url}'><i class='#{item.icono.to_s}'></i>#{item.nombre}</a></li>"
+				codigo << "<li><a href='#{item.url}'><i class='#{item.icono.to_s}'></i><span>#{item.nombre}</span></a></li>"
 			end
 		end
 		codigo << "</ul>"
